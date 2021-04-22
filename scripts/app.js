bb5649a4b4df0758c21da15afb7d198c43db5fbb
neha-s25 <neha.s25@gmail.com>
2021-04-21 18:37:44 -0700

    Game page HTML and CSS added. Started work on JS.

--- scripts/app.js
+++ scripts/app.js
@@ -1,13 +1,42 @@
 console.log("Hi! I am Ghosty...boo");
 let petName;
+let petGhost;
 
 $('#coffin-btn').click(function() {
     window.location.href = 'pages/name-form.html';
 });
 
+function generateRandomInRange(lowerBound, upperBound) {
+    return Math.floor(Math.random()*(upperBound-lowerBound+1)) + lowerBound;
+}
+class Pet {
+    constructor(name) {
+        this.name = name;
+        this.age = generateRandomInRange(1000, 5000);
+        this.hunger = 5;
+        this.sleepiness = 5;
+        this.boredom = 5;
+    }
+}
+
 // When pet's name is entered, take to game page
 $('#name-form').on('submit', function(event) {
     event.preventDefault();
+    console.log('petName Before: ' + petName);
     petName = $('#name-textbox').val();
+    console.log('petName After: ' + petName);
+
+    petGhost = new Pet(petName);
+
     window.location.href = '../pages/game.html';
+    event.preventDefault();
+    $('#name-val-label').text(petGhost.name);
+    $('#age-val-label').text(petGhost.age);
+    console.log(`Writing pet name: ${petGhost.name}`);
 });
+
+
+
+// $(document.window.).ready(function(){
+
+// });
