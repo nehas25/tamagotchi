c24c457696f27c06fb08726a4b40eeb87d81d6bd
neha-s25 <neha.s25@gmail.com>
2021-04-22 10:58:27 -0700

    bug fix in Play functionality

--- scripts/app.js
+++ scripts/app.js
@@ -1,39 +1,55 @@
 console.log("Hi! I am Ghosty...boo");
-let petName;
-let petGhost;
-// const pet = 'PET';
-// const petName = 'PETNAME';
 
-const boredomOutcomes = [
-    {
-        scoreChange: -1,
-        commentaary: `Great job! Your pet is having a lot of fun!!!`
-    },
-    {
-        scoreChange: 1, 
-        commentaary: `You tried to play fetch with your pet. It did not like it. It wants to play Haunt instead.`
-    }
-]
-
-$('#coffin-btn').click(function() {
-    window.location.href = 'pages/name-form.html';
-});
+const petNameKey = 'PETNAME';
+const petKey = 'PET';
 
 function generateRandomInRange(lowerBound, upperBound) {
     return Math.floor(Math.random()*(upperBound-lowerBound+1)) + lowerBound;
 }
 
-function updatePetBoredom(scoreChange) {
-    const pet = JSON.parse(localStorage.getItem('pet'));
-    pet.boredom += scoreChange;
-    console.log(`Pet after score change: `, pet);
-    localStorage.setItem('pet', JSON.stringify(pet));
-    return pet.boredom;
+const boredomOutcomes = [
+    {
+        scoreChange: -1,
+        commentary: `Great job! Your pet is having a lot of fun!!!`
+    },
+    {
+        scoreChange: 1, 
+        commentary: `You tried to play fetch with your pet. It did not like it. It wants to play Haunt instead.`
+    }
+]
+
+//When coffin button is clicked, navigate to Name Form page
+$('#coffin-btn').click(function() {
+    window.location.href = 'pages/name-form.html';
+});
+
+//Function to store from local storage
+function storeItem(keyName, value) {
+    if(keyName === petNameKey) {
+        localStorage.setItem(petNameKey, value);
+    }
+    if(keyName === petKey) {
+        localStorage.setItem(petKey, JSON.stringify(value));
+    }
 }
 
-function displayNewScore(label, newVal) {
-        $(label).text(newVal);
+//Function to retrieve item from local storage
+function getItem(keyName) {
+    if(keyName === petNameKey) {
+        return localStorage.getItem(petNameKey);
+    }
+    if(keyName === petKey) {
+        return new Pet(JSON.parse(localStorage.getItem(petKey)));
+    }
 }
+
+// When pet's name is entered, store name in local storage and navigate to game page
+$('#name-form').on('submit', function(event) {
+    event.preventDefault();
+    let nameEntered = $('#name-textbox').val();
+    storeItem(petNameKey, nameEntered);
+    window.location.href = '../pages/game.html';
+});
 class Pet {
     constructor(name) {
         this.name = name;
@@ -44,42 +60,38 @@ class Pet {
     }
 }
 
-function play() {
+$('#play-button').on('click', function() {
     const boredomOutcome = boredomOutcomes[generateRandomInRange(0, boredomOutcomes.length-1)];
     console.log(boredomOutcome);
     const newBoredomVal = updatePetBoredom(boredomOutcome.scoreChange);
     displayNewScore('#boredom-val-label', newBoredomVal);
-}
-
-// When pet's name is entered, take to game page
-$('#name-form').on('submit', function(event) {
-    event.preventDefault();
-    petName = $('#name-textbox').val();
-    localStorage.setItem('petName', petName);
-    window.location.href = '../pages/game.html';
 });
 
-function displayStats(petGhost) {
-    $('#name-val-label').text(petGhost.name);
-    $('#age-val-label').text(petGhost.age);
-    $('#hunger-val-label').text(petGhost.hunger);
-    $('#sleepiness-val-label').text(petGhost.sleepiness);
-    $('#boredom-val-label').text(petGhost.boredom);
+function updatePetBoredom(scoreChange) {
+    const pet = getItem(petKey);
+    pet.boredom += scoreChange;
+    storeItem(petKey, pet);
+    return pet.boredom;
+}
+
+function displayNewScore(label, newVal) {
+        $(label).text(newVal);
+}
+
+function displayStats(petObj) {
+    $('#name-val-label').text(petObj.name);
+    $('#age-val-label').text(petObj.age);
+    $('#hunger-val-label').text(petObj.hunger);
+    $('#sleepiness-val-label').text(petObj.sleepiness);
+    $('#boredom-val-label').text(petObj.boredom);
 }
 
 window.addEventListener("load", function(){
     if (window.location.pathname === '/pages/game.html') {
         console.log("Game loaded!");
-        const petName = localStorage.getItem('petName');
-        console.log(petName);
-        const petGhost = new Pet(petName);
-        localStorage.setItem('pet', JSON.stringify(petGhost));
-        displayStats(petGhost);
+        const petObj = new Pet(getItem(petNameKey));
+        storeItem(petKey, JSON.stringify(petObj));
+        displayStats(petObj);
     }
 });
 
-$('#play-button').on('click', function() {
-    const pet = JSON.parse(window.localStorage.getItem('pet'));
-    console.log(pet)
-    play();
-});
